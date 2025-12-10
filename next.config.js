a66961577fde82273946bdc08a85b5133254c479
LOSTXKER <saruth05@hotmail.com>
2025-12-11 06:37:59 +0700

    Fix: Remove static export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-    };
-    return config;
   },
 }
 
